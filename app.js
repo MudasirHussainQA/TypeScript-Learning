bb29dac4f772b77d229ce5d6dcdba6a836e93110
Mudasir Hussain <mudsrhussain@yahoo.com>
2019-10-12 22:24:02 +0530

    Basic Start

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("It works!");
+//int//
+var myRealAge;
+myRealAge = 30;
+console.log(myRealAge);
+//string//
+var myName;
+myName = 'Mudasir Hussain';
+console.log(myName);
+//boolean//
+var hasHobbies = true;
+console.log(hasHobbies);
+//assigning types//
+var myAge;
+myAge = 10;
+console.log(myAge);
+myAge = 'Age';
+console.log(myAge);
--- app.ts
+++ app.ts
@@ -1,1 +1,25 @@
 console.log("It works!");
+
+
+//int//
+let myRealAge : number;
+myRealAge=30;
+console.log(myRealAge);
+
+
+//string//
+let myName : string;
+myName='Mudasir Hussain';
+console.log(myName);
+
+//boolean//
+
+let hasHobbies : boolean=true;
+console.log(hasHobbies);
+
+//assigning types//
+let myAge : any;
+myAge=10;
+console.log(myAge);
+myAge='Age';
+console.log(myAge);
